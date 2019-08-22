33a64e5cf364b07f15880d0be1b1add351c57fb7
Shannon Berry <berry65@purdue.edu>
2019-08-22 17:09:17 -0400

    team finished

---
+++ team.java
@@ -0,0 +1,7 @@
+public class team {
+    public static void main(String[] args) {
+        System.out.println("Team: Kannon");
+        System.out.println("\nShannon Berry\nshannonberry");
+        System.out.println("\nKate Brady\nkatebrady8");
+    }
+}
